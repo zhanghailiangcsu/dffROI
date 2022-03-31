1d7e0e1fab4cc7f06552ed050fb55dafa8f2dbae
zhanghailiangcsu <54698235+zhanghailiangcsu@users.noreply.github.com>
2022-03-31 09:25:52 +0800

    Update different_instruments.py

--- different_instruments.py
+++ different_instruments.py
@@ -30,8 +30,8 @@ def FPR(y_true,y_pred):
 if __name__ == '__main__':
     
     _custom_objects = {"TPR":TPR, "FPR":FPR}
-    fusedPDNet_model = tf.keras.models.load_model('D:/code/evaluation/fusedPDNet.h5',custom_objects=_custom_objects)
-    fusedPDNet_model.compile(loss=losses.binary_crossentropy,
+    dffROI_model = tf.keras.models.load_model('D:/code/evaluation/dffROI.h5',custom_objects=_custom_objects)
+    dffROI_model.compile(loss=losses.binary_crossentropy,
             metrics=[BinaryAccuracy(),TPR,FPR])
     peakonly_model = tf.keras.models.load_model('D:/code/evaluation/peakonly.h5',custom_objects=_custom_objects)
     peakonly_model.compile(loss=losses.binary_crossentropy,
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     feature2 = np.load('E:/evaluation/train_data/icr/feature2.npy')
     label2 = np.load('E:/evaluation/train_data/icr/label2.npy')
     result1 = peakonly_model.evaluate(intensity2, label2)
-    result2 = fusedPDNet_model.evaluate([intensity2,feature2], label2)
+    result2 = dffROI_model.evaluate([intensity2,feature2], label2)
     result3 = handcrafted_model.evaluate(feature2, label2)
     
     #test on orbitrap
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     feature2 = np.load('E:/evaluation/train_data/orbitrap/feature2.npy')
     label2 = np.load('E:/evaluation/train_data/orbitrap/label2.npy')
     result4 = peakonly_model.evaluate(intensity2, label2)
-    result5 = fusedPDNet_model.evaluate([intensity2,feature2], label2)
+    result5 = dffROI_model.evaluate([intensity2,feature2], label2)
     result6 = handcrafted_model.evaluate(feature2, label2)
     
     #test on gc
@@ -62,5 +62,5 @@ if __name__ == '__main__':
     feature2 = np.load('E:/evaluation/train_data/gc/feature2.npy')
     label2 = np.load('E:/evaluation/train_data/gc/label2.npy')
     result7 = peakonly_model.evaluate(intensity2, label2)
-    result8 = fusedPDNet_model.evaluate([intensity2,feature2], label2)
+    result8 = dffROI_model.evaluate([intensity2,feature2], label2)
     result9 = handcrafted_model.evaluate(feature2, label2)
