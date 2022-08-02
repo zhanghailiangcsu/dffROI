50ed802ac77c14516d809f0bbbd8f2782e1036fb
zhanghailiangcsu <2352434994@qq.com>
2022-08-02 14:03:46 +0800

    Evaluation metrices

---
+++ train/Evaluation_Metrics.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 12:12:09 2022
+
+@author: Administrator
+"""
+import tensorflow as tf
+
+def TPR(y_true,y_pred): 
+    TP=tf.reduce_sum(y_true*tf.round(y_pred))
+    TN=tf.reduce_sum((1-y_true)*(1-tf.round(y_pred)))
+    FP=tf.reduce_sum((1-y_true)*tf.round(y_pred))
+    FN=tf.reduce_sum(y_true*(1-tf.round(y_pred)))
+    precision=TP/(TP+FN)
+    return precision
+
+def FPR(y_true,y_pred): 
+    TP=tf.reduce_sum(y_true*tf.round(y_pred))
+    TN=tf.reduce_sum((1-y_true)*(1-tf.round(y_pred)))
+    FP=tf.reduce_sum((1-y_true)*tf.round(y_pred))
+    FN=tf.reduce_sum(y_true*(1-tf.round(y_pred)))
+    precision=FP/(TN+FP)
+    return precision
